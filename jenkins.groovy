b4d8155651ed960b9363531bd71f98e4a7979fc9
JaydeepUniverse <ecjaydeepsoni@gmail.com>
2020-07-05 12:26:51 +0530

    check mark for fields in Jira and Jenkins job availability

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,40 +2,76 @@ import com.atlassian.jira.component.ComponentAccessor
 import com.opensymphony.workflow.InvalidInputException
 
 
+log.warn "=== Script Started ==="
+
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
 def repoName = customFieldManager.getCustomFieldObjectByName("Git repositories")
 def pipelinename = customFieldManager.getCustomFieldObjectByName("CI BuildJob")
+log.warn pipelinename
+log.warn pipelinename.getClass()
 def techstackname = customFieldManager.getCustomFieldObjectByName("Tech Stack")
+log.warn techstackname
+log.warn techstackname.getClass()
 def repoNameValue = issue.getCustomFieldValue(repoName)?.value
+log.warn "repoNameValue: "+repoNameValue
 def piplelineNamevalue = issue.getCustomFieldValue(pipelinename)?.value
+log.warn "piplelineNamevalue: "+piplelineNamevalue
+log.warn piplelineNamevalue.getClass()
 def techstackNameValue = issue.getCustomFieldValue(techstackname)?.value
-def BUILD_TOKEN = "11268982e8e7f9d983d79b37bbeb5e4d1d"
-def urlConnection = new URL("http://3.14.131.151:8080/job/DSL_Demo_Seed/buildWithParameters?token=${BUILD_TOKEN}&Gitrepositories=${repoNameValue}&techstack=${techstackNameValue}&name_job_create=${piplelineNamevalue}").openConnection()
+log.warn techstackNameValue.getClass()
+log.warn "techstackNameValue: "+techstackNameValue
+def BUILD_TOKEN = "xxxx"
+log.warn "BUILD_TOKEN: "+BUILD_TOKEN
+def urlConnection = new URL("http://x.x.x.x:8080/job/DSL_Demo_Seed/buildWithParameters?token=${BUILD_TOKEN}&Gitrepositories=${repoNameValue}&techstack=${techstackNameValue}&name_job_create=${piplelineNamevalue}").openConnection()
+log.warn "urlConnection: "+urlConnection
 def message = "{\"name\": \"${piplelineNamevalue}\", \"description\": \"This is your first pipleline\"}"
-def authString = "admin:11268982e8e7f9d983d79b37bbeb5e4d1d".getBytes().encodeBase64().toString()
-def out = new StringBuffer()
-def err = new StringBuffer()
+log.warn "message: "+message
+def authString = "admin:xxxx".getBytes().encodeBase64().toString()
+log.warn "authString: "+authString
 
-def command = "curl -XGET \"http://3.14.131.151:8080/checkJobName?value=${piplelineNamevalue}\" --user admin:${BUILD_TOKEN}"
+
+def command = "curl -XGET \"http://x.x.x.x:8080/checkJobName?value=${piplelineNamevalue}\" --user admin:${BUILD_TOKEN}"
+log.warn "command: "+command
 Process process = command.execute()
+log.warn "process: "+process
+def out = new StringBuffer()
+log.warn "out: "+out
+def err = new StringBuffer()
+log.warn "err: "+err
 process.consumeProcessOutput( out, err )
 process.waitFor()
+log.warn "out: "+out
+log.warn "err: "+err
+log.warn "out size: "+out.size()
 
-if( out.size() == 7 ) {
-    urlConnection.setDoOutput(true)
-    urlConnection.setRequestMethod("POST")
-    urlConnection.setRequestProperty("Authorization", "Basic ${authString}")
-    urlConnection.setRequestProperty("Content-Type", "application/json")
-    def postRC = urlConnection.getResponseCode();
 
-    println(urlConnection.getInputStream().getText())
-    if(postRC.equals(201)) {
-        println(post.getInputStream().getText());      
+if (!piplelineNamevalue) {
+    invalidInputException = new InvalidInputException("PipelineName Is Not Provided")
+    def block4 = "block - PipelineName Is Not Provided"
+    log.warn block4
+} else if (!techstackNameValue) {
+    invalidInputException = new InvalidInputException("Either TechStackName Is Not Provided")
+    def block4 = "block - TechStackName Is Not Provided"
+    log.warn block4
+} else if( out.size() == 7 ) {
+        def block1 = "In out size if block"
+        log.warn block1
+        urlConnection.setDoOutput(true)
+        urlConnection.setRequestMethod("POST")
+        urlConnection.setRequestProperty("Authorization", "Basic ${authString}")
+        urlConnection.setRequestProperty("Content-Type", "application/json")
+        def postRC = urlConnection.getResponseCode();
+
+        println(urlConnection.getInputStream().getText())
+        if(postRC.equals(201)) {
+            println(post.getInputStream().getText());      
+        } else {
+            invalidInputException = new InvalidInputException("Jenkins Pipeline Creation failed.")
+        }
+    } 
+    else{
+        def block2 = "In out size else block, Jenkins Job Already Exist"
+        log.warn block2
+        invalidInputException = new InvalidInputException("Jenkins Job Already Exist")
     }
-    else 
-        invalidInputException = new InvalidInputException("Jenkins Pipeline Creation failed.")
-}else{
-    invalidInputException = new InvalidInputException("Jenkins Pipeline Creation failed.")
-}
-
 log.warn "=== Script Ended ==="
